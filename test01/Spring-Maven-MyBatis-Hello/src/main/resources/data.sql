90f399579f8833ed9bfb69a5da3891e9a2adad58
grtlinux <grtlinux@gmail.com>
2019-03-12 00:46:01 +0900

    create 1st

--- test01/Spring-Maven-MyBatis-Hello/src/main/resources/data.sql
+++ test01/Spring-Maven-MyBatis-Hello/src/main/resources/data.sql
@@ -11,4 +11,5 @@ CREATE TABLE `test`(
 INSERT INTO TEST(id, pw, NAME) VALUES ( 'id1', 'pw1', 'name1');
 INSERT INTO TEST(id, pw, NAME) VALUES ( 'id2', 'pw2', 'name2');
 INSERT INTO TEST(id, pw, NAME) VALUES ( 'id3', 'pw3', 'name3');
+INSERT INTO TEST(id, pw, NAME) VALUES ( 'id4', 'pw4', '한글4');
 
